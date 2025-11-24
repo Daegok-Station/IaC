975b0f2ae92c975fb643d0757e3a42f148c1382d
jbbok89 <jbbok89@gmail.com>
2025-11-24 16:38:52 +0900

    Create task_definition.tf

---
+++ CICD/ecs/task_definition.tf
@@ -0,0 +1,50 @@
+#################################################################################
+################################ ECS Task Definition ############################
+#################################################################################
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family = "Daegok-Petclinic-task"
+
+  network_mode = "awsvpc"   # Fargate 실행을 위한 네트워크 모드 awsvpc
+
+  requires_compatibilities = ["FARGATE"]
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = "arn:aws:iam::954382416992:role/ecsTaskExecutionRole" # iam rope arn
+
+  # 컨테이너 정의
+  container_definitions = <<TASK_DEFINITION
+[
+  {
+    "cpu": 0,
+    "essential": true,
+    "image": "${aws_ecr_repository.ecr.repository_url}:latest",  # ⚡ Jenkins push 이미지 자동 참조
+    "name": "web",
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080,
+        "protocol": "tcp"
+      }
+    ],
+    "environment": [
+      {
+        "name": "ENVIRONMENT",
+        "value": "dev"                      
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "/ecs/Daegok-Petclinic-task",  
+        "awslogs-create-group": "true",
+        "awslogs-region": "ap-northeast-2",         
+        "awslogs-stream-prefix": "ecs"
+      }
+    }
+  }
+]
+TASK_DEFINITION
+}
