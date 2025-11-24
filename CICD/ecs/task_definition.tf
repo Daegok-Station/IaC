108547c356306f36744392cba0e6eac35d252fd7
jbbok89 <jbbok89@gmail.com>
2025-11-24 19:27:58 +0900

    Update task_definition.tf

--- CICD/ecs/task_definition.tf
+++ CICD/ecs/task_definition.tf
@@ -4,14 +4,10 @@
 
 resource "aws_ecs_task_definition" "task_definition" {
   family = "Daegok-Petclinic-task"
-
   network_mode = "awsvpc"   # Fargate 실행을 위한 네트워크 모드 awsvpc
-
   requires_compatibilities = ["FARGATE"]
-
   cpu    = "256"
   memory = "512"
-
   execution_role_arn = "arn:aws:iam::954382416992:role/ecsTaskExecutionRole" # iam rope arn
 
   # 컨테이너 정의
@@ -20,7 +16,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   {
     "cpu": 0,
     "essential": true,
-    "image": "${aws_ecr_repository.ecr.repository_url}:latest",  # ⚡ Jenkins push 이미지 자동 참조
+    "image": "${aws_ecr_repository.ecr.repository_url}:latest",  # Jenkins push 이미지 자동 참조
     "name": "web",
     "portMappings": [
       {
