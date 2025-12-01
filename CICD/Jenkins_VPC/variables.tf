2dd0608e5fdf4fbf1181048f386ef87e47e305f6
jbbok89 <jbbok89@gmail.com>
2025-12-01 17:02:20 +0900

    Update variables.tf

--- CICD/Jenkins_VPC/variables.tf
+++ CICD/Jenkins_VPC/variables.tf
@@ -4,35 +4,33 @@
 
 variable "Jenkins_VPC_cidr" {
   type = string          
-  default = "10.0.0.0/16"         # VPC IPv4 주소 범위
-  description = "Jenkins VPC CIDR"      # Terraform 문서화 및 CLI 출력용 설명
+  default = "10.0.0.0/16"               # VPC IPv4 주소 범위
+  description = "Jenkins VPC CIDR"   
 }
 
 #################################################################################
 ################################ Subnet CIDR ####################################
 #################################################################################
 
-############################### Public subnet CIDR ##############################
+############################# Public Subnet CIDR ################################
 
 variable "bastion_subnet_cidr" {
   type = string
-  default = "10.0.1.0/24"       # Public Subnet IPv4 주소 범위
+  default = "10.0.1.0/24"         # Public Subnet IPv4 주소 범위
   description = "Daegok-public-subnet-1 CIDR"
 }
 
-############################### Private subnet CIDR ##############################
+############################## Private Subnet CIDR ##############################
 
 variable "jenkins_subnet_cidr" {
   type = string
-  default = "10.0.2.0/24"       # Private Subnet IPv4 주소 범위
+  default = "10.0.2.0/24"         # Private Subnet IPv4 주소 범위
   description = "Daegok-private-subnet-1 CIDR"
 }
 
 #################################################################################
-#################################### AZ #########################################
+######################## Public/Private Subnet AZ ###############################
 #################################################################################
-
-################### Public/Private subnet 가용 영역(AZ) ##########################
 
 variable "availability_zone_Jenkins_VPC" {
   type        = string
