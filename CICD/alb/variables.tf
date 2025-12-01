b8e7dbf28589b7326431730405c8f7034944daed
jbbok89 <jbbok89@gmail.com>
2025-12-01 17:17:39 +0900

    Create variables.tf

---
+++ CICD/alb/variables.tf
@@ -0,0 +1,23 @@
+variable "public_subnet_id_01" {
+  type = string
+  description = "Public subnet for ECS tasks"
+}
+
+variable "public_subnet_id_02" {
+  type = string
+  description = "Public subnet for ECS tasks"
+}
+
+variable "Service_VPC_id" {
+  type = string
+  description = "Service VPC ID"
+}
+
+variable "jenkins_sg_id" {
+  type        = string
+  description = "Jenkins Security Group ID for ALB rules."
+}
+
+variable "Service_VPC_cidr" {
+  type = string
+}
