06c2bf16be6fa2ce5489b0a9c1f53c799cef6208
jbbok89 <jbbok89@gmail.com>
2025-12-01 17:09:09 +0900

    Update security-group.tf

--- CICD/ec2/security-group.tf
+++ CICD/ec2/security-group.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "jenkins_sg" {
     from_port   = 22              # 허용 시작 포트
     to_port     = 22              # 허용 끝 포트
     protocol    = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]  # bastion에서만 접속 가능
+    cidr_blocks = [var.Service_VPC_cidr]  # bastion에서만 접속 가능
   }
 
   ## 아웃바운드 규칙
@@ -71,46 +71,6 @@ resource "aws_security_group" "jenkins_sg" {
 }
 
 #################################################################################
-################################## ALB SG #######################################
-#################################################################################
-
-resource "aws_security_group" "ALB_sg" {
-  name = "Daegok-ALB-SG"                            # SG 이름 (콘솔)
-  description = "Public EC2 security group"     # Public Subnet EC2용 보안 그룹
-  vpc_id = var.Service_VPC_id                       # VPC 참조
-  
-  ## 인바운드 규칙
-  # SSH 허용 (모든 IP)
-  ingress {
-    from_port   = 80              # 허용 시작 포트
-    to_port     = 80             # 허용 끝 포트
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH 허용 (모든 IP)
-  ingress {
-    from_port   = 443              # 허용 시작 포트
-    to_port     = 443              # 허용 끝 포트
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ## 아웃바운드 규칙
-  # 모든 트래픽 허용
-  egress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    security_groups = [aws_security_group.jenkins_sg.id]
-  }
-
-  tags = {
-    Name = "Daegok-ALB-SG"  
-  }
-}
-
-#################################################################################
 ############################### Service SG ######################################
 #################################################################################
 
@@ -122,8 +82,8 @@ resource "aws_security_group" "Service_sg" {
   ## 인바운드 규칙
   # SSH 허용 (모든 IP)
   ingress {
-    from_port   = 8080              # 허용 시작 포트
-    to_port     = 8080              # 허용 끝 포트
+    from_port   = 8080             
+    to_port     = 8080              
     protocol    = "tcp"
     security_groups = [aws_security_group.jenkins_sg.id]  # bastion에서만 접속 가능
   }
