b0dc8bfaef16b3786950d4d982d599e60b1248cf
jbbok89 <jbbok89@gmail.com>
2025-12-02 23:51:04 +0900

    Update alb_listener.tf

--- CICD/alb/alb_listener.tf
+++ CICD/alb/alb_listener.tf
@@ -8,14 +8,3 @@ resource "aws_lb_listener" "blue_listener" {
     target_group_arn = aws_lb_target_group.blue.arn
   }
 }
-
-resource "aws_lb_listener" "green_listener" {
-  load_balancer_arn = aws_lb.Daegok_alb.arn
-  port = 80
-  protocol = "HTTP"
-
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.green.arn
-  }
-}
