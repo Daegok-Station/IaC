70c561046440ea6409e6e8a9c49cfc6f5d88981d
jbbok89 <jbbok89@gmail.com>
2025-12-02 23:49:37 +0900

    Update variables.tf

--- CICD/ec2/variables.tf
+++ CICD/ec2/variables.tf
@@ -35,6 +35,10 @@ variable "jenkins_subnet_id" {
   description = "Jenkins VPC Jenkins Subnet ID"
 }
 
+variable "jenkins_subnet_cidr" {
+ type = string 
+}
+
 #################################################################################
 ################################ key pair ###################
 #################################################################################
@@ -61,16 +65,6 @@ variable "internet_gateway_Jenkins_id" {
 #################################################################################
 ################################## SG #####################################
 #################################################################################
-
-variable "bastion_sg_id" {
-  type = string
-  description = "Bastion Security Group"
-}
-
-variable "jenkins_sg_id" {
-  type = string
-  description = "Jenkins Security Group"
-}
 
 variable "alb_sg_id" {
   type = string
@@ -190,4 +184,65 @@ variable "jenkins_ecs_policy_json" {
   ]
 }
 JSON
+}
+
+# ec2/variables.tf
+
+locals {
+  # Jenkins Agent가 S3에 아티팩트를 업로드하고 CodePipeline 실행을 트리거하기 위한 
+
+  # 1. S3 PutObject 및 GetLocation 권한 정책
+  jenkins_s3_policy_document = <<-EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetBucketLocation"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.artifact_bucket_name}",
+                "arn:aws:s3:::${var.artifact_bucket_name}/*"
+            ]
+        }
+    ]
+}
+EOT
+
+  # 2. CodePipeline 실행 권한 정책
+  jenkins_codepipeline_policy_document = <<-EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "codepipeline:StartPipelineExecution",
+            "Resource": "arn:aws:codepipeline:${var.aws_region}:${var.aws_account_id}:${var.codepipeline_name}"
+        }
+    ]
+}
+EOT
+}
+
+variable "artifact_bucket_name" {
+  description = "Jenkins가 아티팩트를 업로드할 S3 버킷 이름"
+  type        = string
+}
+
+variable "codepipeline_name" {
+  description = "Jenkins가 실행을 요청할 CodePipeline 이름"
+  type        = string
+  default     = "Daegok-Codepipeline"
+}
+
+variable "aws_region" {
+  description = "AWS Region (ap-northeast-2)"
+  type        = string
+}
+
+variable "aws_account_id" {
+  description = "AWS 계정 ID"
+  type        = string
 }
